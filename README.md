3cc9db984b86957b181ec2d2ec6716990e0f14a4
Luke Parry <lukejordanparry@gmail.com>
2024-01-23 23:41:29 +0000

    script

---
+++ README.md
@@ -0,0 +1,7 @@
+load_data.sh:
+    - Installs Algolia CLI (64-bit debian)
+    - Prompts for admin API key, which can be found under https://dashboard.algolia.com/account/api-keys/restricted
+    - Indexes content
+
+'expected algolia payload.json'
+    - Shouldn't usually be committed, only for exercise purposes
--- load_data.sh
+++ load_data.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
 
+APPLICATION_ID="494CB4DJ46"
+FILE="expected algolia payload.json"
+INDEX_NAME="prod_luke"
+ALGOLIA_VERSION="1.5.0"
+
+echo "Checking for Algolia install..."
+
+if ! command -v algolia
+then
+    echo "Installing Algolia CLI version $ALGOLIA_VERSION..."
+    rm algolia_*.deb
+    wget https://github.com/algolia/cli/releases/download/v$ALGOLIA_VERSION/algolia_"$ALGOLIA_VERSION"_linux_amd64.deb
+    sudo dpkg -i algolia_*.deb
+fi
+
+echo "Enter the admin API key for Algolia CLI: https://dashboard.algolia.com/account/api-keys/all?applicationId=$APPLICATION_ID"
+echo
+
+read ADMIN_API_KEY
+
+echo "Loading data from $FILE..."
+
+algolia objects import prod_luke -F "$FILE" --application-id $APPLICATION_ID --admin-api-key $ADMIN_API_KEY
