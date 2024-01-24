aec57c1f21d0c69bdc8f6f02342c046540ec7b1f
Luke Parry <lukejordanparry@gmail.com>
2024-01-24 14:05:13 +0000

    script install

--- load_data.sh
+++ load_data.sh
@@ -4,15 +4,24 @@ APPLICATION_ID="494CB4DJ46"
 FILE="expected algolia payload.json"
 INDEX_NAME="prod_luke"
 ALGOLIA_VERSION="1.5.0"
+ARCHITECTURE="linux_amd64"
 
 echo "Checking for Algolia install..."
 
 if ! command -v algolia
 then
-    echo "Installing Algolia CLI version $ALGOLIA_VERSION..."
-    rm algolia_*.deb
-    wget https://github.com/algolia/cli/releases/download/v$ALGOLIA_VERSION/algolia_"$ALGOLIA_VERSION"_linux_amd64.deb
-    sudo dpkg -i algolia_*.deb
+    echo "Installing Algolia CLI version $ALGOLIA_VERSION for $ARCHITECTURE systems..."
+
+    # Clean up old downloads
+    #rm node-"$NODE_VERSION"
+    rm -r algolia_*.tar.gz
+
+    # Download and export Algolia CLI
+    wget https://github.com/algolia/cli/releases/download/v$ALGOLIA_VERSION/algolia_"$ALGOLIA_VERSION"_$ARCHITECTURE.tar.gz
+    tar -xf algolia_*.tar.gz
+
+    # Install CLI
+    sudo mv algolia_"$ALGOLIA_VERSION"_$ARCHITECTURE/algolia /usr/local/bin
 fi
 
 echo "Enter the admin API key for Algolia CLI: https://dashboard.algolia.com/account/api-keys/all?applicationId=$APPLICATION_ID"
